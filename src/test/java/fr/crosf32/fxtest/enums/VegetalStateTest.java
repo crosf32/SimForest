23657005531b1aa9ffa47bf04542c6385a47b012
MrSnox <oo.snow.wolf.oo@gmail.com>
2019-07-09 11:31:57 +0200

    junit

---
+++ src/test/java/fr/crosf32/fxtest/enums/VegetalStateTest.java
@@ -0,0 +1,20 @@
+package fr.crosf32.fxtest.enums;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VegetalStateTest {
+
+    @Test
+    void getDisplayNumber() {
+    }
+
+    @Test
+    void getRiskOfFire() {
+    }
+
+    @Test
+    void getRiskOfInfection() {
+    }
+}
---
+++ src/test/java/fr/crosf32/fxtest/handler/ForestBuilderTest.java
@@ -0,0 +1,24 @@
+package fr.crosf32.fxtest.handler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ForestBuilderTest {
+
+    @Test
+    void randomGeneration() {
+    }
+
+    @Test
+    void setAt() {
+    }
+
+    @Test
+    void getAt() {
+    }
+
+    @Test
+    void get() {
+    }
+}
